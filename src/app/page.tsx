aafe7518f73d2e94a6d4d89ec3ca4e4639a4bd6e
kiritocode1 <kathawalearyan9@gmail.com>
2023-07-18 17:50:35 +0530

    error 2 ~ auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,50 @@
 import Image from 'next/image'
+import { Inter } from 'next/font/google';
+import { Metadata} from 'next'; 
+import  LargeHeading  from '@/components/ui/LargeHeading';
+import Paragraph from '@/components/ui/paragraph';
+import Link from 'next/link';
+
+
+const author = {
+  URL:'https://github.com/kiritocode1', 
+  name: "Aryan Kathawale || kiritocode1"
+}
+
+
+export const metadata : Metadata= {
+  title: "similarity API | Home", 
+  description: 'FOSS text similarity api ', 
+  authors:author
+} 
+
 
 export default function Home() {
   return (
-    <div>
-      <h1 className='text-bold text-red-700'>
-        hello world 
-      </h1>
+    <div className="relative h-screen flex items-center justify-center overflow-x-hidden">
+      <div className="container pt-32 m-w-7xl mx-auto w-full h-full">
+        <div className="h-full gap-6 flex flex-col justify-start lg:justify-center items-center lg:items-start">
+          <LargeHeading size={'lg'} className='three-d text-black  dark:text-light-gold'>
+            Quickly  Determine <br/> text similarity , for nerds .
+          </LargeHeading>
+
+          <Paragraph className='max-w-xl lg:text-left'>
+            with  the text similarity api , you can easily determine the similarity between two pieces of text with a free  {" "} 
+            <Link href={'/login'} className="underline underline-offset-2 text-black dark:text-light-gold">
+              API Key 
+            </Link>
+            .
+          </Paragraph>
+
+          <div className='relative w-full max-w-lg lg:max-w-3xl  lg:left-1/2 aspect-square lg:absolute '>
+            <Image priority className='img-shadow' quality={100} style={{ objectFit: 'contain' }}
+              fill 
+              src="/typewriter.png"
+              alt='typewriter image'
+            />
+          </div>
+        </div>
+      </div>
     </div>
   )
-}
+};
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,34 +1,31 @@
+
+
+
 import { getServerSession } from 'next-auth'
 import Link from 'next/link';
-import { FC } from 'react'
 import { buttonVariants } from '@/components/ui/Button';
 import SignInButton from '@/components/SignInButton';
 import SignOutButton from '@/components/SignOutButton';
 import ThemeToggle from '@/components/ThemeToggle';
-
+import { authOptions } from '@/lib/auth';
 
 const Navbar = async () => {
-    const session = await getServerSession();
+    const session = await getServerSession(authOptions);
     return (
 			<div className="fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900/75 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between">
 				<div className="container max-w-7xl mx-auto w-full flex justify-between items-center">
 					<Link href="/" className={buttonVariants({ variant: "link" })}>
 						Similarity v1.0
-						<div className="hidden lg:flex">lg</div>
-						<div className="hidden md:flex">md</div>
-						<div className="flex md:hidden">sm</div>
 					</Link>
 
-					<div className="md:hidden">
-						<ThemeToggle />
-					</div>
 
-					<div className="hidden md:flex gap-4">
+
+					<div className=" md:flex gap-4">
 						<ThemeToggle />
 						<Link
 							href="/documentation"
 							className={buttonVariants({ variant: "ghost" })}>
-							Documentation
+							Docs
 						</Link>
 						{session ? (
 							<>
@@ -39,7 +36,7 @@ const Navbar = async () => {
 								</Link>
 								<SignOutButton />
 							</>
-						) : (
+                    ) : (
 							<SignInButton />
 						)}
 					</div>
